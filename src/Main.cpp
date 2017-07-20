7119a383134c706fa1cc53bfc61c5d82b706849f
hschier <hudsonschier@gmail.com>
2017-07-20 10:46:26 -0700

    slight format change to serial debug

--- src/Main.cpp
+++ src/Main.cpp
@@ -44,9 +44,9 @@ void loop() {
 	lambda_time = (10000 * lambda) / (SPEED_OF_SOUND/100); // in ticks
 	sample_time = lambda_time / 120;
 	if (Serial.available()) {
-		Serial.printf("readings:%u %u %u floor_dist:%u sample_time:%u\
-			lambda:%u lambda_time:%u i:%u timer_read-sample_start:%u\n",
-		readings[0], readings[1], readings[2], floor_dist, sample_time,
+		Serial.printf("\nreadings:%u %u %u floor_dist:%u sample_time:%u\n",
+		readings[0], readings[1], readings[2], floor_dist, sample_time);
+		Serial.printf("lambda:%u lambda_time:%u i:%u delta:%u\n",
 		lambda, lambda_time, i, timer_read(1) - sample_start);
 		// flush serial
 		while(Serial.available()) Serial.read();
